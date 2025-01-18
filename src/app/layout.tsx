a11cab40fc31f82fa939d440bb45688d550e6fb2
Doug <doug.edgates@gmail.com>
2025-01-18 20:19:53 -0300

    add .editorconfig and more configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 export const metadata = {
-    title: 'Vânia Costura Criativa',
-    description: 'Transformando suas idéias em realidade.',
+	title: 'Studio Chifrezz',
+	description: 'Animation Studio',
 }
 
-    export default function RootLayout({
-    children,
-    }: {
-    children: React.ReactNode
-    }) {
-    return (
-        <html lang="pr-BR">
-            <body>{children}</body>
-        </html>
-    )
+	export default function RootLayout({
+	children,
+	}: {
+	children: React.ReactNode
+	}) {
+	return (
+		<html lang="pr-BR">
+			<body>{children}</body>
+		</html>
+	)
 }
