016bae287b9171e13f96b1a4644547291d4faa73
Doug <doug.edgates@gmail.com>
2025-01-26 20:11:23 -0300

    feat: add About page

---
+++ src/pages/about/styles.ts
@@ -0,0 +1,56 @@
+import { screenSizes } from "@/styles/GlobalStyles";
+import styled from "styled-components";
+
+export const AboutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8em;
+  max-width: ${screenSizes.desktopL};
+  width: ${screenSizes.laptop};
+  padding: 4em 0;
+`
+
+export const StudioDescription = styled.h2`
+  width: 800px;
+  font-weight: 400;
+  text-align: justify;
+`
+
+export const Us = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4em;
+`
+
+export const Member = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2em;
+  text-align: justify;
+
+  img {
+    min-width: 16em;
+    border-radius: 50%;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+  }
+`
+
+export const MemberReverse = styled(Member)`
+  flex-direction: row-reverse;
+  align-items: center;
+
+  div {
+    align-items: flex-end;
+  }
+`
