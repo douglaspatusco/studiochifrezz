29b8663e258bfbc89667a15853a6ece2ad07b013
Doug <doug.edgates@gmail.com>
2025-01-31 00:46:21 -0300

    feat: install and config Yup

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,33 +1,38 @@
-import { useContactForm } from '../../hooks/useContactForm'
+import { useContactFormSec } from '../../hooks/useContactFormSec'
 import * as S from '../../styles/contact.styles'
 
 export const ContactForm = () => {
-  const { formData, status, handleChange, handleSubmit } = useContactForm()
+  const { register, handleSubmit, errors, isSubmitting, status } = useContactFormSec()
 
   return (
     <S.Form onSubmit={handleSubmit}>
       <label htmlFor="name">Nome</label>
-      <S.Input id="name" type="text" placeholder="Nome" value={formData.name} onChange={handleChange} />
+      <S.Input id="name" type="text" placeholder="Nome" {...register("name")} />
+      {errors.name && <p>{errors.name.message}</p>}
 
       <label htmlFor="email">Email</label>
-      <S.Input id="email" type="text" placeholder="Email" value={formData.email} onChange={handleChange} />
+      <S.Input id="email" type="text" placeholder="Email" {...register("email")} />
+      {errors.email && <p>{errors.email.message}</p>}
 
       <label htmlFor="subject">Assunto</label>
-      <S.Input id="subject" type="text" placeholder="Assunto" value={formData.subject} onChange={handleChange} />
+      <S.Input id="subject" type="text" placeholder="Assunto" {...register("subject")} />
+      {errors.subject && <p>{errors.subject.message}</p>}
 
       <label htmlFor="message">Mensagem</label>
-      <S.TextArea id="message" placeholder="Mensagem" value={formData.message} onChange={handleChange} />
+      <S.TextArea id="message" placeholder="Mensagem" {...register("message")} />
+      {errors.message && <p>{errors.message.message}</p>}
 
-      <S.Button type="submit">ENVIAR</S.Button>
-      {status === 'loading' && <p>Enviando...</p>}
-      {status === 'success' && (
+      <S.Button type="submit" disabled={isSubmitting}>ENVIAR</S.Button>
+
+      {status === "loading" && <p>Enviando...</p>}
+      {status === "success" && (
         <p>
           Email enviado com sucesso!
           <br />
           Entraremos em contato em breve.
         </p>
       )}
-      {status === 'error' && <p>Erro ao enviar o email. Tente novamente.</p>}
+      {status === "error" && <p>Erro ao enviar o email. Tente novamente.</p>}
     </S.Form>
   )
 }
