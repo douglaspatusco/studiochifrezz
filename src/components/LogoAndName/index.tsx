9aba6e7c20ed1adfd8ef2239ed0f3e8e841b56a4
Doug <doug.edgates@gmail.com>
2025-02-27 15:36:06 -0300

    change image files to a smal size

--- src/components/LogoAndName/index.tsx
+++ src/components/LogoAndName/index.tsx
@@ -3,17 +3,20 @@ import StudioChifrezz from '@/components/StudioChifrezz'
 import Image from 'next/image'
 
 import { Container } from './styles'
+import Link from 'next/link'
 
 const LogoAndName = () => {
   return (
     <Container>
-      <Image
+      <Link href='/'>
+        <Image
         src={'/favicon.ico'}
         alt="Studio Chifrezz Logotype"
         width={100}
         height={100}
         priority={true}
       />
+      </Link>
       <StudioChifrezz />
     </Container>
   )
