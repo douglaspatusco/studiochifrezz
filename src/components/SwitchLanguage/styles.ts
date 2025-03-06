b6e87eb95f94ba178a78fe96a55864b62059fe9a
Doug <doug.edgates@gmail.com>
2025-03-06 18:38:14 -0300

    change flags images in switch language

--- src/components/SwitchLanguage/styles.ts
+++ src/components/SwitchLanguage/styles.ts
@@ -48,18 +48,19 @@ export const Slider = styled.span<{ $isEN: boolean }>`
 
   &::before {
     position: absolute;
-    content: ${({ $isEN }) => ($isEN ? '"🇺🇸"' : '"🇧🇷"')};
+    content: "";
     height: 40px;
     width: 40px;
     transition:
       transform 0.4s ease,
       background-color 0.4s ease;
     border-radius: 50%;
-    font-size: 25px;
     display: flex;
     justify-content: center;
     align-items: center;
-    background: white; /* Adiciona um fundo branco para destacar o emoji */
+    background: white no-repeat center/cover;
+    background-image: ${({ $isEN }) =>
+      $isEN ? 'url("/images/icons/flag-eua.png")' : 'url("/images/icons/flag-brasil.png")'};
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
     transform: ${({ $isEN }) => ($isEN ? 'translateX(56px)' : 'translateX(0)')};
   }
